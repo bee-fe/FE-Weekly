1f3e6cde342e0a73251bb0adfcd2c52ca8c4a79d
时间小鱼 <grantcolemans@gmail.com>
2019-08-29 17:25:26 +0800

    :wrench: 引入评论功能

---
+++ docs/components/comment/comment.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="gitalk-container">
+    <div id="gitalk-container"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "comment",
+  data() {
+    return {};
+  },
+  mounted() {
+    const linkGitalk = document.createElement("link");
+    linkGitalk.href = "https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css";
+    linkGitalk.rel = "stylesheet";
+    document.body.appendChild(linkGitalk);
+    let body = document.querySelector(".gitalk-container");
+    let script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js";
+    body.appendChild(script);
+    script.onload = () => {
+      const commentConfig = {
+        clientID: "5d586ac3b9a1825cd400",
+        clientSecret: "7836f8301165c3a7243066fbd508a36db3b28f7c",
+        repo: "FE-Weekly",
+        owner: "G-Grant",
+        admin: ["G-Grant"],
+        id: location.pathname,
+        distractionFreeMode: false
+      };
+      const gitalk = new Gitalk(commentConfig);
+      gitalk.render("gitalk-container");
+    };
+  }
+};
+</script>
